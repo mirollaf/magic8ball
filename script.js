faba16ccf33eab08c01358e81aad6405db9d2cff
mirollaf <mirollaf07@gmail.com>
2023-12-18 13:48:58 -0500

    progress 12/18 class

--- script.js
+++ script.js
@@ -1,7 +1,68 @@
-function shakeMagic8Ball(){
-    const question=prompt(`Ask The Magic 8-Ball A Question`)
-    if(question===null){
-document.getElementById(`response-text`).innerText=`ASL A QUESTION DAWG!!`
-document.getElementById(`response-text`).classList= `text-danger`
+function shakeMagic8Ball() {
+    const question = prompt('Ask the Magic 8-Ball a question:');
+    if (question === null) {
+        document.getElementById('response-text').innerText = 'ASK A QUESTION BUD!'
+        document.getElementById('response-text').classList = 'text-danger'
+        document.getElementById('response-image').src = '#BAD/BROKENIMAGE' //FIX THIS
+        return
     }
+
+    if (!question.trim()) {
+        alert('Please enter a valid question!')
+        return
     }
+
+    const randomNumber = Math.floor(Math.random() * 8);
+    let answer, image, color;
+
+    switch (randomNumber) {
+        case 0:
+            answer = "YUP";
+            image = "yes-image.jpg" ;
+            color = "text-success";
+            break;
+        case 1:
+            answer = "NOPE!";
+            image = "thumbs-down.webp";
+            color = "text-danger";
+            break;
+        case 2:
+            answer = "Ask again later";
+            image = "later-image.jpg" //change this!!;
+            color = "text-secondary";
+            break;
+        case 3:
+            answer = "Maybe";
+            image = "maybe-image.jpg";//change this!!;
+            color = "text-warning";
+            break;
+        case 4:
+            answer = "Cannot predict now";
+            image = "cannot-predict-image.jpg";//change this!!;
+            color = "text-muted";
+            break;
+        case 5:
+            answer = "HA YOU WISH";
+            image = "dont-count-on-it-image.jpg";//change this!!;
+            color = "text-danger";
+            break;
+        case 6:
+            answer = "Most likely";
+            image = "most-likely-image.jpg";//change this!!;
+            color = "text-success";
+            break;
+        case 7:
+            answer = "Outlook not so good";
+            image = "outlook-not-good-image.jpg";//change this!!;
+            color = "text-danger";
+            break;
+        default:
+            break;
+    }
+
+const responseText = document.getElementById('response-text');
+responseText.innerText = answer;
+responseText.classList = `display-4 ${color}`;
+document.getElementById('response-image').src = image;
+
+}
